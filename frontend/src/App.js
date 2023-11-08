b22af6e24e322667035b6408e8ecaba81a5a1d20
Ananya <ananyaraj24@gmail.com>
2023-11-08 12:02:53 +0530

    features

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import './App.css';
 
-import Nav from './components/Nav';
 import Home from './components/Home';
 import Model from './components/Model'
 import FastAPI from './components/FastAPI';
@@ -11,7 +10,6 @@ function App() {
   return (
     <div>
       <BrowserRouter>
-        <Nav />
         <div className='app'>
           <Routes>
             <Route path='/' element={<Home />} />
